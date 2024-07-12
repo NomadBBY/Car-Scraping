c37997f7bd3934d43d2bb45ccf72e0ab69d6193f
Artis_Code <89861525+NomadBBY@users.noreply.github.com>
2024-07-12 11:10:28 +0300

    New changes

--- main.py
+++ main.py
@@ -1,35 +1,79 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
-def fetch_soup(website_url):
-    response = requests.get(website_url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, "html.parser")
-        return soup
-    else:
-        print(f"Unable to fetch page: {website_url}")
+class CarScraper:
+    def __init__(self, base_url):
+        self.base_url = base_url
+
+    def fetch_soup(self, url):
+        response = requests.get(url)
+        if response.status_code == 200:
+            return BeautifulSoup(response.content, "html.parser")
+        print(f"Unable to fetch page: {url}")
         return None
 
-def fetch_div(soup, class_name):
-    if soup:
-        div = soup.find('div', class_=class_name)
-        return div
-    return None
+    def fetch_car_info(self, soup):
+        cars_container = soup.find('div', class_="cars-container row-3")
+        if not cars_container:
+            return []
+        
+        car_info_list = []
+        car_elements = cars_container.find_all('div', class_="cars-container__car")
+        
+        for car in car_elements:
+            title_element = car.find('h2', class_="cars__title")
+            subtitle_element = car.find('p', class_="cars__subtitle")
+            
+            if title_element and subtitle_element:
+                link_element = title_element.find('a')
+                if link_element:
+                    title = link_element.text.strip()
+                    link = link_element['href']
+                    subtitle = subtitle_element.text.strip()
+                    car_info_list.append({
+                        'title': title,
+                        'link': link,
+                        'subtitle': subtitle
+                    })
+        
+        return car_info_list
+
+    def scrape_cars(self, params=None):
+        url = self.base_url
+        if params:
+            url += '?' + '&'.join([f"{k}={v}" for k, v in params.items()])
+        
+        soup = self.fetch_soup(url)
+        if not soup:
+            return None
+
+        return self.fetch_car_info(soup)
+
+def print_car_info(car_info_list):
+    if car_info_list:
+        print(f"Found {len(car_info_list)} car(s):")
+        for i, car in enumerate(car_info_list, 1):
+            print(f"Car {i}:")
+            print(f"Title: {car['title']}")
+            print(f"Link: {car['link']}")
+            print(f"Subtitle: {car['subtitle']}")
+            print("-" * 40)
+    else:
+        print("No car information found")
 
 if __name__ == "__main__":
-    link = "https://lv.brcauto.eu/lietoti-auto?city=5&search=1&makes=31473"
+    base_url = "https://lv.brcauto.eu/lietoti-auto"
+    scraper = CarScraper(base_url)
     
-    soup = fetch_soup(link)
-    if soup:
-
-        print("Connection Made")
-        print(f"Title of the page: {soup.title.string}")
-        div = fetch_div(soup, "cars-container row-3")
-        if div:
-            print("Found the div with class 'cars-container row-3'")
-            print(div)
-        else:
-            print("Div with class 'cars-container row-3' not found")
-            
+    # Example usage
+    params = {
+        "city": "5",
+        "search": "1",
+        "makes": "31473"
+    }
+    
+    car_info = scraper.scrape_cars(params)
+    if car_info is not None:
+        print_car_info(car_info)
     else:
         print("Failed to fetch the page")
